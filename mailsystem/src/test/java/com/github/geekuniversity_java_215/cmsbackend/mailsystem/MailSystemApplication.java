4d452a461087121f50553aa8fafba7f77dd3463c
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-04-24 04:55:05 +0300

    remove unused

--- mailsystem/src/test/java/com/github/geekuniversity_java_215/cmsbackend/mailsystem/MailSystemApplication.java
+++
@@ -1,7 +0,0 @@
-package com.github.geekuniversity_java_215.cmsbackend.mailsystem;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// Required to run tests (create Spring Boot Configuration)
-@SpringBootApplication
-public class MailSystemApplication {}
