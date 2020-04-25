ac6757a9976c17468a78f601548730d16695ef5c
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-04-25 04:20:28 +0300

    implementing init script

--- install-properties.sh
+++ install-properties.sh
@@ -1,18 +1,60 @@
 #!/bin/sh
 
-fromPath=core/src/main/resources/properties-default/
+# cmsapp
+fromPath=cms-backend/cmsapp/src/main/resources/
+FROM=${fromPath}application.properties
+TO=${fromPath}application-dev.properties
 
-for f in ${fromPath}*.default; do
+cp -an $FROM $TO
 
-    #This line splits the file name on the last delimiter "."
-#baseName=`echo $f | cut -d "." -f 1`
-    baseName=`echo $f | grep -Po '.*(?=\.)'`
-    #echo "baseName "${baseName}
-    #one level up
-    toPath=`dirname "${baseName}"`/..
-    #echo "toPath "${toPath}
-    echo "copy_rename "$f
-    cp $f ${baseName}
-    # do not move over existing file
-    mv -n ${baseName} ${toPath}
-done
+if [[ ! -f "$TO" ]]; then
+    sed -i 's/logback-spring.xml/logback-spring-dev.xml/g' $TO
+fi
+
+
+
+# core
+fromPath=cms-backend/core/src/main/resources/
+cp -an ${fromPath}core.properties ${fromPath}core-dev.properties
+
+
+
+# mail
+fromPath=cms-backend/mail/src/main/resources/
+FROM=${fromPath}mail.properties
+TO=${fromPath}mail-dev.properties
+
+if [[ ! -f "$FROM" ]]; then
+
+    echo -e "#mail settings\n" >> $TO
+    echo -e "mail.username=vasya@pypkin.com\n" >> $TO
+    echo -e "mail.password=vasyapassword\n" >> $TO
+
+    cp -an $FROM $TO
+fi
+
+
+
+
+# payment
+fromPath=cms-backend/payment/src/main/resources/
+#cp -an ${fromPath}payment.properties ${fromPath}payment-dev.properties
+
+
+
+# utils
+fromPath=cms-backend/utils/src/main/resources/
+FROM=${fromPath}logback-spring.xml
+TO=${fromPath}logback-spring-dev.xml
+
+if [[ ! -f "$TO" ]]; then
+
+    cp -an $FROM $TO
+    sed -i 's/<logger name=\"org.hibernate.SQL\" level=\"OFF\"\/>/<logger name=\"org.hibernate.SQL\" level=\"TRACE\"\/>/g' $TO
+    
+fi
+
+
+
+
+
