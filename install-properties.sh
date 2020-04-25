5deffa4c0659e8f3a4222ab264188786c811a366
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-04-25 04:51:53 +0300

    fixed init script

--- install-properties.sh
+++ install-properties.sh
@@ -1,34 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 # cmsapp
-fromPath=cms-backend/cmsapp/src/main/resources/
+fromPath=cmsapp/src/main/resources/
 FROM=${fromPath}application.properties
 TO=${fromPath}application-dev.properties
 
-cp -an $FROM $TO
-
-if [[ ! -f "$TO" ]]; then
+if [ ! -f "$TO" ]; then
+    cp -an $FROM $TO
     sed -i 's/logback-spring.xml/logback-spring-dev.xml/g' $TO
 fi
 
 
 
 # core
-fromPath=cms-backend/core/src/main/resources/
-cp -an ${fromPath}core.properties ${fromPath}core-dev.properties
+fromPath=core/src/main/resources/
+FROM=${fromPath}core.properties
+TO=${fromPath}core-dev.properties
+
+cp -an $FROM $TO
 
 
 
 # mail
-fromPath=cms-backend/mail/src/main/resources/
+fromPath=mail/src/main/resources/
 FROM=${fromPath}mail.properties
 TO=${fromPath}mail-dev.properties
 
-if [[ ! -f "$FROM" ]]; then
+if [ ! -f "$FROM" ]; then
 
-    echo -e "#mail settings\n" >> $TO
-    echo -e "mail.username=vasya@pypkin.com\n" >> $TO
-    echo -e "mail.password=vasyapassword\n" >> $TO
+    echo -e "#mail settings" >> $FROM
+    echo -e "mail.username=vasya@pypkin.com" >> $FROM
+    echo -e "mail.password=vasyapassword" >> $FROM
 
     cp -an $FROM $TO
 fi
@@ -37,24 +39,19 @@ fi
 
 
 # payment
-fromPath=cms-backend/payment/src/main/resources/
+fromPath=payment/src/main/resources/
 #cp -an ${fromPath}payment.properties ${fromPath}payment-dev.properties
 
 
 
 # utils
-fromPath=cms-backend/utils/src/main/resources/
+fromPath=utils/src/main/resources/
 FROM=${fromPath}logback-spring.xml
 TO=${fromPath}logback-spring-dev.xml
 
-if [[ ! -f "$TO" ]]; then
+if [ ! -f "$TO" ]; then
 
     cp -an $FROM $TO
     sed -i 's/<logger name=\"org.hibernate.SQL\" level=\"OFF\"\/>/<logger name=\"org.hibernate.SQL\" level=\"TRACE\"\/>/g' $TO
-    
 fi
-
-
-
-
 
