98924d2094ad6be60c754c4db9e8f627257c94b0
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-04-27 02:17:58 +0300

    fix install-properties script

--- install-properties.sh
+++ install-properties.sh
@@ -1,43 +1,63 @@
 #!/bin/bash
 
-# cmsapp
+LIB_MODULES=(core mail payment utils)
+
+# Prepare logging config fom library modules ------------------------
+
+# cp application.properties application-dev.properties in <module>/test/main/resources/
+# inside replace logback-spring.xml to logback-spring-dev.xml
+create_tests_dev () {
+    fromPath=$1/src/test/resources/
+    FROM=${fromPath}application.properties
+    TO=${fromPath}application-dev.properties
+
+    if [[ ! -f "$TO" ]]; then
+        cp -an $FROM $TO
+        sed -i 's/logback-spring.xml/logback-spring-dev.xml/g' $TO
+    fi
+}
+
+# execute it for all library modules
+for var in "${LIB_MODULES[@]}"
+do
+  create_tests_dev ${var}
+done
+
+
+# cmsapp -----------------------------------------------------------
 fromPath=cmsapp/src/main/resources/
 FROM=${fromPath}application.properties
 TO=${fromPath}application-dev.properties
 
-if [ ! -f "$TO" ]; then
+if [[ ! -f "$TO" ]]; then
     cp -an $FROM $TO
     sed -i 's/logback-spring.xml/logback-spring-dev.xml/g' $TO
 fi
 
-
-
-# core
+# core -----------------------------------------------------------
 fromPath=core/src/main/resources/
 FROM=${fromPath}core.properties
 TO=${fromPath}core-dev.properties
 cp -an $FROM $TO
 
 
-# configuration
+# configuration -----------------------------------------------------------
 fromPath=configuration/src/main/resources/
 FROM=${fromPath}logback-spring.xml
 TO=${fromPath}logback-spring-dev.xml
-if [ ! -f "$TO" ]; then
+if [[ ! -f "$TO" ]]; then
 
     cp -an $FROM $TO
     sed -i 's/<logger name=\"org.hibernate.SQL\" level=\"OFF\"\/>/<logger name=\"org.hibernate.SQL\" level=\"TRACE\"\/>/g' $TO
     sed -i 's/<logger name=\"com.github.geekuniversity_java_215.cmsbackend\" level=\"INFO\"\/>/<logger name=\"com.github.geekuniversity_java_215.cmsbackend\" level=\"TRACE\"\/>/g' $TO
 fi
 
-
-
-# mail
+# mail -----------------------------------------------------------
 fromPath=mail/src/main/resources/
 FROM=${fromPath}mail.properties
 TO=${fromPath}mail-dev.properties
 
-if [ ! -f "$FROM" ]; then
+if [[ ! -f "$FROM" ]]; then
 
     echo -e "#mail settings" >> $FROM
     echo -e "mail.username=vasya@pypkin.com" >> $FROM
@@ -49,9 +69,11 @@ fi
 
 
 
-# payment
+# payment -----------------------------------------------------------
 fromPath=payment/src/main/resources/
-#cp -an ${fromPath}payment.properties ${fromPath}payment-dev.properties
+ # NOT IMPLEMENTED
+
+
 
 
 
