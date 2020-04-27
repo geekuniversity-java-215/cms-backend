7bfea459dd9332f3171709c64983a74380481648
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-04-27 23:13:26 +0300

    CORS allow * ORIGIN

--- core/src/main/java/com/github/geekuniversity_java_215/cmsbackend/core/configuration/SpringConfiguration.java
+++ core/src/main/java/com/github/geekuniversity_java_215/cmsbackend/core/configuration/SpringConfiguration.java
@@ -1,7 +1,40 @@
 package com.github.geekuniversity_java_215.cmsbackend.core.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 @Configuration
 public class SpringConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+
+        // ObjectMapper is threadsafe
+
+        // allow convertation to/from Instant
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        //mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+        return mapper;
+    }
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        return factory.getValidator();
+    }
 }
