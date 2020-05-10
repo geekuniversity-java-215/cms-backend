ef6ed945da7124c64b201968e95690e976280333
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-05-10 15:33:55 +0300

    authserver server.port=8080 -> 8090, refactoring cmsapplication.configuration.WebSecurityConfig

--- cmsapp/src/main/java/com/github/geekuniversity_java_215/cmsbackend/cmsapplication/configuration/WebSecurityConfig.java
+++ cmsapp/src/main/java/com/github/geekuniversity_java_215/cmsbackend/cmsapplication/configuration/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.github.geekuniversity_java_215.cmsbackend.cmsapplication.configurati
 
 import com.github.geekuniversity_java_215.cmsbackend.core.configurations.filtres.BearerRequestResourceFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,42 +12,19 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final BearerRequestResourceFilter bearerRequestResourceFilter;
 
-    /**
-     * Resource access
-     * Authorisation: Bearer (access_token)
-     */
-    @Configuration
-    public static class AdminWebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        private final BearerRequestResourceFilter bearerRequestResourceFilter;
-
-        @Autowired
-        public AdminWebSecurityConfig(BearerRequestResourceFilter bearerRequestResourceFilter) {
-            this.bearerRequestResourceFilter = bearerRequestResourceFilter;
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable()
-                .addFilterAfter(bearerRequestResourceFilter, LogoutFilter.class);
-        }
+    @Autowired
+    public WebSecurityConfig(BearerRequestResourceFilter bearerRequestResourceFilter) {
+        this.bearerRequestResourceFilter = bearerRequestResourceFilter;
     }
-  
-/*
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-
-        http.antMatcher("/api/**").anonymous()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().csrf().disable();
+        http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .addFilterAfter(bearerRequestResourceFilter, LogoutFilter.class);
     }
-*/
-
 }
