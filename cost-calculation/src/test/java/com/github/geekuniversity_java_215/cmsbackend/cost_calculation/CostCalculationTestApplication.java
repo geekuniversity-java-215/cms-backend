890b413eceedbcde7933fac1d028d9faa3e90683
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-05-13 16:40:01 +0300

    merged dev_shipping_cost_calculation

---
+++ cost-calculation/src/test/java/com/github/geekuniversity_java_215/cmsbackend/cost_calculation/CostCalculationTestApplication.java
@@ -0,0 +1,7 @@
+package com.github.geekuniversity_java_215.cmsbackend.cost_calculation;
+
+import com.github.geekuniversity_java_215.cmsbackend.core.configurations.MultimoduleSpringBootApplication;
+
+@MultimoduleSpringBootApplication
+public class CostCalculationTestApplication {
+}
