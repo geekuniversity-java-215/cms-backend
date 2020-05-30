bbc14e9c6ebf024a1ba3d2361abfbd39db1eb4a8
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-05-30 20:52:11 +0300

    jenkins fix take 12

--- ztests/scripts/2-system-tests.sh
+++ ztests/scripts/2-system-tests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# subsystem tests
-mvn -DskipTests package
-
 # auth-server
 java ${POSTGRESQL_PARAMS} -jar auth-server/target/auth-server-0.1.jar & PID1=$(echo $!)
 # cms-app
 java ${POSTGRESQL_PARAMS} -jar cmsapp/target/cms-app-0.1.jar & PID2=$(echo $!)
-sleep 2;
 
+
+# subsystem tests
+mvn -DskipTests package
+
+sleep 2;
 # check that process is running
 kill -0 $PID1
 kill -0 $PID2
