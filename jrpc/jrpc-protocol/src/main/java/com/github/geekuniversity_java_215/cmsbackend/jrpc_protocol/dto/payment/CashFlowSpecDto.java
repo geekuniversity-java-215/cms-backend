d53d1002dda7aee8897239396a38337537c17647
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-06-06 23:37:21 +0300

    investigating cashflow dto

--- jrpc/jrpc-protocol/src/main/java/com/github/geekuniversity_java_215/cmsbackend/jrpc_protocol/dto/payment/CashFlowSpecDto.java
+++ jrpc/jrpc-protocol/src/main/java/com/github/geekuniversity_java_215/cmsbackend/jrpc_protocol/dto/payment/CashFlowSpecDto.java
@@ -1,5 +1,6 @@
 package com.github.geekuniversity_java_215.cmsbackend.jrpc_protocol.dto.payment;
 
+import com.github.geekuniversity_java_215.cmsbackend.jrpc_protocol.dto.user.UserDto;
 import lombok.Data;
 
 import java.time.Instant;
@@ -7,7 +8,8 @@ import java.time.Instant;
 @Data
 public class CashFlowSpecDto {
 
-    private Long user_id;
+    private Long id;
+    private UserDto user;
     private Instant startDate;
     private Instant endDate;
 }
